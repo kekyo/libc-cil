5b9d1321771a011c316509e6345160a4a38976cf
Kouji Matsui <k@kekyo.net>
2024-05-01 20:04:57 +0900

    Added crt0 files.

---
+++ crt0/crt0_i_v.s
@@ -0,0 +1,7 @@
+; libc-cil - libc implementation on CIL, part of chibicc-cil
+; Copyright (c) Kouji Matsui(@kozy_kekyo, @kekyo @mastodon.cloud)
+; Licensed under MIT: https://opensource.org/licenses/MIT
+
+.function __module__ int32() _start
+	call main
+	ret
